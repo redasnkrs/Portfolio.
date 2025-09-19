cf8f4c66056af92938ed2856893a3ce71b25f7c1
reda. <redadahmani34@gmail.com>
2025-09-19 15:42:06 +0200

    added a json file for my todoo list

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,14 +3,14 @@ import { ContactMe } from "../components/ContactMe";
 import { ExperienceSection } from "../components/Experience";
 import { FloatingNav } from "../components/FloatingNav";
 import { Hero } from "../components/Hero";
-import { Profile } from "../components/Profile";
 import { Projects } from "../components/Projects";
+// import { Profile } from "../components/Profile";
 // import { TodoList } from "../components/TodoList";
 
 export function HomePage() {
   return (
     <div>
-      <Profile />
+      {/*<Profile />*/}
       <Hero />
       <About />
       <ExperienceSection />
