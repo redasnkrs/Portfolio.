a9f39a867a6f99391c0407da9efb09f8a873ad05
reda. <redadahmani34@gmail.com>
2025-09-19 08:17:41 +0200

    Minor change, added json file for contact.

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,13 +1,25 @@
+import { contactMe } from "../data/contact.json";
+
 export function ContactMe() {
   return (
-    <section id='contactMe' className='section'>
-      <div className=''>
-        <h2 className='text-4xl text-center mb-10'>Contact me</h2>
-        <div className='container-4xl glass-card'>
-          <p className='py-5  '>If you wanna know more about me feel free to contact me ! </p>
-          <div className='flex flex-wrap gap-3'>
-            <a className="pill hover:bg-[rgba(255,255,255,0.08)]" href="mailto:redadahman34@gmail.com">Email</a>
-            <a className="pill hover:bg-[rgba(255,255,255,0.08)]" href="phoneto:0488981759">Phone</a>
+    <section id="contactMe" className="section">
+      <div className="">
+        <h2 className="text-4xl text-center mb-10">Contact me</h2>
+        <div className="container-4xl glass-card">
+          <p className="py-5  ">{contactMe}</p>
+          <div className="flex flex-wrap gap-3">
+            <a
+              className="pill hover:bg-[rgba(255,255,255,0.08)]"
+              href="mailto:redadahman34@gmail.com"
+            >
+              Email
+            </a>
+            <a
+              className="pill hover:bg-[rgba(255,255,255,0.08)]"
+              href="phoneto:0488981758"
+            >
+              Phone
+            </a>
           </div>
         </div>
       </div>
--- src/data/sections.ts
+++ src/data/sections.ts
@@ -1,10 +1,15 @@
-import { Home, FolderGit2, BriefcaseBusiness, User2 } from 'lucide-react';
+import { Home, FolderGit2, BriefcaseBusiness, User2 } from "lucide-react";
 
-export type SectionId = 'home' | 'projects' | 'experience' | 'about';
+export type SectionId = "home" | "projects" | "experience" | "about"; //| "goals";
 
-export const sections: { id: SectionId; label: string; icon: React.ComponentType<{ className?: string }> }[] = [
-  { id: 'home', label: 'Home', icon: Home },
-  { id: 'about', label: 'About', icon: User2 },
-  { id: 'experience', label: 'Experience', icon: BriefcaseBusiness },
-  { id: 'projects', label: 'Projects', icon: FolderGit2 },
+export const sections: {
+  id: SectionId;
+  label: string;
+  icon: React.ComponentType<{ className?: string }>;
+}[] = [
+  { id: "home", label: "Home", icon: Home },
+  { id: "about", label: "About", icon: User2 },
+  { id: "experience", label: "Experience", icon: BriefcaseBusiness },
+  { id: "projects", label: "Projects", icon: FolderGit2 },
+  //{ id: "goals", label: "Goals", icon: FolderGit2 },
 ];
