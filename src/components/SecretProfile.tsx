88e71219e3bdc327e296cdee66d515389add64db
reda. <redadahmani34@gmail.com>
2025-09-23 12:51:36 +0200

    feat: added a new font

--- src/components/SecretProfile.tsx
+++ src/components/SecretProfile.tsx
@@ -6,7 +6,10 @@ export function SecretProfile() {
           <h2 className="text-[64px] leading-none text-[rgba(255,255,255,0.79)]">
             My Profile
           </h2>
-          <h3>Name:</h3>
+          <div>
+            <h3>Name:</h3>
+            <p className="font-cusom">Reda Dahmani</p>
+          </div>
         </div>
       </div>
     </section>
