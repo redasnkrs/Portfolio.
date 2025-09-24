8c3e5b1f8e5a16f0d5a01b1fd0f7296133ac73de
reda. <redadahmani34@gmail.com>
2025-09-24 16:52:57 +0200

    feat: enhance secret profile with bio and social links

--- src/components/SecretProfile.tsx
+++ src/components/SecretProfile.tsx
@@ -1,18 +1,61 @@
 export function SecretProfile() {
   return (
-    <section id="" className="relative h-screen flex items-center bg-black ">
+    <section
+      id=""
+      className="relative h-screen flex justify-center items-center bg-black  "
+    >
       <div className="absolute">
-        <div className="glass-card flex flex-col justify-between gap-50  ">
-          <h2 className="text-[64px] leading-none text-[rgba(255,255,255,0.79)]">
+        <div className="glass-card flex flex-col justify-between gap-8   ">
+          <h2 className="text-[64px] mb-5 leading-none text-[rgba(255,255,255,0.79)]">
             My Profile
           </h2>
-          <div>
-            <h3>Name:</h3>
-            <p className="font-custom">Reda Dahmani</p>
-          </div>
-          <div>
-            <h3>Age:</h3>
-            <p className="fon-custom">22 years old</p>
+          <div className="flex flex-col justify-between gap-5">
+            <div>
+              <h3>Name:</h3>
+              <p className="font-custom">Reda Dahmani</p>
+            </div>
+            <div>
+              <h3>Age:</h3>
+              <p className="fon-custom">22 years old</p>
+            </div>
+            <div>
+              <h3>Title/Headline:</h3>
+              <p className="font-custom">
+                Full-stack developper, Software developper
+              </p>
+            </div>
+            <div>
+              <h3>Bio:</h3>
+              <p className="font-custom">
+                I build and scale web application from concept to completion.
+              </p>
+            </div>
+            <div>
+              <h3>My social links:</h3>
+              <a
+                className="pill hover:bg-[rgba(255,255,255,0.08)]"
+                href="https://github.com/redasnkrs"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub
+              </a>
+
+              <a
+                className="pill hover:bg-[rgba(255,255,255,0.08)]"
+                href="https://linkedin.com/reda-dahmani/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Linkdedin
+              </a>
+              <a
+                className="pill hover:bg-[rgba(255,255,255,0.08)]"
+                href="mailto:redadahmani34@gmail.com"
+              >
+                Email
+              </a>
+            </div>
           </div>
         </div>
       </div>
