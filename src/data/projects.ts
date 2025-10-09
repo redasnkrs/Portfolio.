4ff5d43edc6e2a3f847fe98aa6bb3c9195cdab94
reda. <redadahmani34@gmail.com>
2025-10-09 08:09:27 +0200

    del: del img path

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,5 +1,4 @@
 export type Project = {
-  image: string;
   title: string;
   description: string;
   technologies: string[];
@@ -8,19 +7,16 @@ export type Project = {
 
 export const projects: Project[] = [
   {
-    image: "../assets/lan.jpeg",
     title: "Kikotch",
     description: "Built a WordPress website.",
     technologies: ["PHP", "HTML", "CSS", "JavaScript"],
   },
   {
-    image: "../assets/lan.jpeg",
     title: "GhostFrom (Developpement)",
     description: "Full-stack website i built with a friend from scratch.",
     technologies: ["Next.js", "React", "Prisma"],
   },
   {
-    image: "../assets/lan.jpeg",
     title: "LuxyCar (Developpement)",
     description: "The next level when is about renting a car.",
     technologies: ["React", "TypeScript", "NodeJs"],
