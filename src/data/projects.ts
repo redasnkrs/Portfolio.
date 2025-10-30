5bc2d26029af96b0ae80628f8c7546badfa3274d
reda. <redadahmani34@gmail.com>
2025-10-30 08:26:22 +0100

    Revert "fix: Changed tech in project."
    This reverts commit b73bcb28890cd8aab95a677a41e751a286439acd.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -8,21 +8,20 @@ export type Project = {
 export const projects: Project[] = [
   {
     title: "Kikotch",
-    description: "Built a project from scratch on Wordpress.",
-    technologies: ["HTML", "CSS", "JavaScript"],
+    description: "Built a WordPress website.",
+    technologies: ["PHP", "HTML", "CSS", "JavaScript"],
     link: " https://kitakotch.de",
   },
   {
     title: "KitaPay",
     description:
       "Built in Wordpress. We took a old website and covert it in wp.",
-    technologies: ["HTML", "CSS", "JavaScript"],
+    technologies: ["Next.js", "React", "Prisma"],
     link: "https://kitapay.de/",
   },
   {
     title: "LuxyCar (Developpement)",
     description: "The next level when is about renting a car.",
-    technologies: ["React", "TypeScript", "Synfony"],
-    link: "../pages/LuxyCar.tsx",
+    technologies: ["React", "TypeScript", "NodeJs"],
   },
 ];
